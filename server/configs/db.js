e6b67a35b5133938959b0e58bc24828f0c047658
Bhikadiya <bh957167@dal.ca>
2025-09-10 11:25:55 -0400

    update DB configs

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://manav:manav123@cluster0.b0mouiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
