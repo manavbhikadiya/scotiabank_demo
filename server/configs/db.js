95af86f887131f813c9d856709d73e70f0e8d592
Bhikadiya <bh957167@dal.ca>
2025-09-10 11:52:16 -0400

    update CICD pipeline

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://manav:manav123@cluster0.b0mouiy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connection successfull.");
   } catch (error) {
     console.error(
-      "Error: In connecting database. Please check db.js file for the connection configs."
+      "Error: In connecting database. Please check db.js file for the connection configs.", error
     );
   }
 };
